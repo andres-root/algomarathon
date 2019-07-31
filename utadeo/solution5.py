43e229a90ae6eac4b49840e787257d82dece3893
Andres Lujan <andreslujandev@gmail.com>
2019-07-30 23:15:01 -0500

    WIP algoritms

---
+++ utadeo/solution5.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def merge(l):
+    if n == 1:
+        return True
+    elif n < 1:
+        return False
+    
+    return power_of_two(n/2)
+
+def merge_sort(a, b):
+
+def test(arr):
+    return [power_of_two(n) for n in arr]
+
+
+if __name__ == '__main__':
+    arr = [x for x in range(1, 11)]
+    print(list(zip(arr, test(arr))))
