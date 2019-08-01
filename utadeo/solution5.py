2cd8332a69f350ddb9ab68a6be0db9f661ec5af1
Andres Lujan <andreslujandev@gmail.com>
2019-07-31 20:33:51 -0500

    WIP algoritms

--- utadeo/solution5.py
+++ utadeo/solution5.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python3
 
 
-def merge(l):
-    if n == 1:
-        return True
-    elif n < 1:
-        return False
+def merge(l, r):
+    m = []
+    li = 0
+    ri = 0
+
+    while li < len(l) and ri < len(r):
+        if l[li] > r[ri]:
+            m.append(r[ri])
+            ri += 1
+        else:
+            m.append(l[li])
+            li += 1
+
+    m += l[li:]
+    m += r[ri:]
+
+    return m
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    m = len(arr)//2
     
-    return power_of_two(n/2)
+    l = merge_sort(arr[:m])
+    r = merge_sort(arr[m:])
 
-def merge_sort(a, b):
+    return merge(l, r)
 
-def test(arr):
-    return [power_of_two(n) for n in arr]
+
+def test_function(test_cases):
+    results = []
+
+    for arr in test_cases:
+        results.append(merge_sort(arr))
+    
+    return results
 
 
 if __name__ == '__main__':
-    arr = [x for x in range(1, 11)]
-    print(list(zip(arr, test(arr))))
+    arr = [2, 5, 7, 3, 1, 8, 9]
+    test_cases = [arr]
+    print(test_function(test_cases))
